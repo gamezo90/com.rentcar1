b5c7902efefaa00f333aba836e4ec6b51c162b3e
Nick <otulan@example.com>
2022-10-31 21:08:38 +0300

    12

---
+++ api/src/main/java/com/rentcar/controller/springdata/CarController.java
@@ -0,0 +1,29 @@
+package com.rentcar.controller.springdata;
+
+
+import com.rentcar.repository.CarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rest/data/car")
+public class CarController {
+
+    private final CarRepository repository;
+
+    @GetMapping
+    public ResponseEntity<Object> findAllCars() {
+
+        return new ResponseEntity<>(
+                Collections.singletonMap("result", repository.findAll()),
+                HttpStatus.OK
+        );
+    }
+}
---
+++ common/src/main/java/com/rentcar/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package com.rentcar.repository;
+
+import com.rentcar.domain.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+//    List<Car> findAll();
+}
