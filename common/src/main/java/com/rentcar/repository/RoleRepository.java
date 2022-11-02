cb3c0ec2013160d84cc593e3c89791a4829200f9
Nick <gamezo90@mail.ru>
2022-11-02 19:59:31 +0300

    I'am bandit

--- common/src/main/java/com/rentcar/repository/RoleRepository.java
+++ common/src/main/java/com/rentcar/repository/RoleRepository.java
@@ -2,6 +2,7 @@ package com.rentcar.repository;
 
 
 import com.rentcar.domain.Role;
+import com.rentcar.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,8 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
     @Query(value = "select r from Role  r" +
             " inner join User u on r.id = u.id ")
     List<Role> findRolesByUserId(Long userId);
+
+    @Query("select r from Role r inner join r.users u where u.id = :userId")
+    List<Role> findRolesByUserId1(Long userId);
+
 }
