9e430da9241d9831fb57599cd4d669d5f2bc6fdc
Nick <gamezo90@mail.ru>
2022-11-15 20:21:46 +0300

    Change UserMapper

--- api/src/main/java/com/rentcar/controller/mappers/UserMapper.java
+++ api/src/main/java/com/rentcar/controller/mappers/UserMapper.java
@@ -1,12 +1,15 @@
 package com.rentcar.controller.mappers;
 
 import com.rentcar.controller.requests.UserCreateRequest;
+import com.rentcar.controller.response.UserResponse;
 import com.rentcar.domain.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper
 public interface UserMapper {
+    @Mapping(source = "roles", target = "userRoles")
+    UserResponse toResponse(User user);
 
     @Mapping(source = "login",target = "credentials.Login")
     @Mapping(source = "password",target = "credentials.Password")
