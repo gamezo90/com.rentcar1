b7ff780d42f44ab39b0875cf0881dfb07551ce8d
Nick <gamezo90@mail.ru>
2022-11-15 19:58:50 +0300

    Add UserResponse

---
+++ api/src/main/java/com/rentcar/controller/response/UserResponse.java
@@ -0,0 +1,37 @@
+package com.rentcar.controller.response;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+
+public class UserResponse {
+
+    private long id;
+
+    private String userName;
+
+    private String surname;
+
+    private boolean isBanned;
+
+    private boolean isDeleted;
+
+    private String region;
+
+    private Timestamp birthday;
+
+    private String gender;
+
+    private String userLogin;
+
+    private String userPassword;
+
+    private String userEmail;
+
+}
+
+
+
+
