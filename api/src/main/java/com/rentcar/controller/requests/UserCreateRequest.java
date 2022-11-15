ab01a2edd10a01077a8005534da8cdd5a4988e4a
Nick <gamezo90@mail.ru>
2022-11-15 19:45:08 +0300

    Fixed UserCreateRequest

--- api/src/main/java/com/rentcar/controller/requests/UserCreateRequest.java
+++ api/src/main/java/com/rentcar/controller/requests/UserCreateRequest.java
@@ -13,37 +13,24 @@ import java.sql.Timestamp;
 @ApiModel(description = "Create user object without system info")
 public class UserCreateRequest {
 
-    @ApiModelProperty(required = true, allowableValues = "slavka", dataType = "string", notes = "user's login")
-    @NotNull
     @Size(min = 2, max = 20)
     private String userName;
 
-    @ApiModelProperty(required = true, allowableValues = "kalevich", dataType = "string", notes = "user's surname")
-    @NotNull
     @Size(min = 2, max = 50)
     private String surname;
 
-    @ApiModelProperty(required = true, allowableValues = "hgfdhdcghd", dataType = "string", notes = "user's region")
     private String region;
 
-    @ApiModelProperty(required = true, allowableValues = "1665685166000", dataType = "timestamp", notes = "user's birth")
     private Timestamp birthday;
 
-    @ApiModelProperty(required = true, allowableValues = "MALE, FEMALE, NOT_SELECTED", dataType = "string", notes = "user's gender")
     private Gender gender;
 
-    @ApiModelProperty(required = true, allowableValues = " ", dataType = "string", notes = "user's login")
     @Size(min = 2,max = 100)
     private String login;
 
-    @ApiModelProperty(required = true, allowableValues = "", dataType = "string", notes = "user's password")
     @Size(min = 2,max = 255)
     private String password;
 
-    @ApiModelProperty(required = true, allowableValues = "", dataType = "string", notes = "user's email")
     @Size(min = 2,max = 255)
     private String email;
-
-
-
 }
