6f7282f6dc0445abc4a02cb8d17fd4ce91a2c37c
Nick <gamezo90@mail.ru>
2022-11-15 20:14:42 +0300

    Fixed UserMapper

--- api/src/main/java/com/rentcar/controller/mappers/UserMapper.java
+++ api/src/main/java/com/rentcar/controller/mappers/UserMapper.java
@@ -1,7 +1,15 @@
 package com.rentcar.controller.mappers;
 
+import com.rentcar.controller.requests.UserCreateRequest;
+import com.rentcar.domain.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface UserMapper {
+
+    @Mapping(source = "login",target = "credentials.Login")
+    @Mapping(source = "password",target = "credentials.Password")
+    @Mapping(source = "email",target = "credentials.email")
+    User convertCreateRequest(UserCreateRequest userCreateRequest);
 }
